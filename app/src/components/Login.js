cf07fb2eb306f58443455cd19d138067ab7b0803
Jacevedo21 <jacevedo0213@gmail.com>
2020-07-27 15:17:17 -0400

    Possibly complete login/signup

--- app/src/components/Login.js
+++ app/src/components/Login.js
@@ -1,9 +1,34 @@
 import React from 'react'
 
+
+const initialLoginForm = {
+    username: '',
+    password: '',
+}
+
+const initialLoginError = {
+    username: '', 
+    password: '',
+}
+
 export default function Login() {
+
     return (
         <div>
-            
+            <form>
+                <label>username
+                    <input 
+                    name='username'
+                    type='text'
+                    />
+                </label>
+                <label>password
+                    <input 
+                    name='password'
+                    type='text'
+                    />
+                </label>
+            </form>
         </div>
     )
 }
