f584218840abe9800b19bbcec693e861ae651b09
Jacevedo21 <jacevedo0213@gmail.com>
2020-07-27 15:12:20 -0400

    completed signup

--- app/src/components/Signup.js
+++ app/src/components/Signup.js
@@ -1,81 +1,118 @@
 import React, { useState, useEffect } from 'react'
-import axios from 'axios'
-import * as yup from 'yup'
-
-const initialSignupForm = {
-    name: '',
-    email: '',
-    password: '',
-}
-const initialError = {
-    name: '',
-    email: '',
-    password: '',
-}
 
 
-const initialSignup = [];
-const initialDisabled = true;
+// const initialSignupForm = {
+//     name: '',
+//     email: '',
+//     password: '',
+// }
+// const initialError = {
+//     name: '',
+//     email: '',
+//     password: '',
+// }
+
+
+// const initialSignup = [];
+// const initialDisabled = true;
 
 
 export default function Signup() {
 
-    const [data, setData] = useState(initialSignup)
-    const [signup, setSignup] = useState(initialSignupForm)
-    const [error, setError] = useState(initialError)
-    const [disabled, setDisabled] = useState(initialDisabled)
+    // const [data, setData] = useState(initialSignup)
+    // const [signup, setSignup] = useState(initialSignupForm)
+    // const [error, setError] = useState(initialError)
+    // const [disabled, setDisabled] = useState(initialDisabled)
 
-    useEffect(() => {
-        const postData = newData => {
-            axios.post('', newData)
-            .then(res => {
-                setData([...data, res.data])
-                setSignup(initialSignupForm)
-            })
-            .catch(err => {
-                setSignup(initialSignupForm)
-            })
-        }
-    })
+    // useEffect(() => {
+    //     const postData = newData => {
+    //         axios.post('', newData)
+    //         .then(res => {
+    //             setData([...data, res.data])
+    //             setSignup(initialSignupForm)
+    //         })
+    //         .catch(err => {
+    //             setSignup(initialSignupForm)
+    //         })
+    //     }
+    // })
 
-    // cosnt inputChange = (name, value) => {
-    //     yup
-    //     .reach(formChema, name)
-    //     .validate(value)
-    //     .then(valid => {
-    //         setError({
-    //             ...error,
-    //             [name]: "",
-    //         })
-    //     })
-    //     .catch(err => {
-    //         setError({
-    //             ...error,
-    //             [name]: err.errors[0]
-    //         })
-    //     })
-    //     setSignup({
-    //         ...signup,
-    //         [name]: value
-    //     })
+    // // cosnt inputChange = (name, value) => {
+    // //     yup
+    // //     .reach(formChema, name)
+    // //     .validate(value)
+    // //     .then(valid => {
+    // //         setError({
+    // //             ...error,
+    // //             [name]: "",
+    // //         })
+    // //     })
+    // //     .catch(err => {
+    // //         setError({
+    // //             ...error,
+    // //             [name]: err.errors[0]
+    // //         })
+    // //     })
+    // //     setSignup({
+    // //         ...signup,
+    // //         [name]: value
+    // //     })
+    // // }
+
+    // const submit = () => {
+    //     const newSignup = {
+    //         name: signup.name.trim(),
+    //         email: signup.email.trim(),
+    //         password: signup.password.trim()
+    //     }
+    //     postData(newData)
     // }
-    
-    const submit = () => {
-        const newSignup = {
-            name: signup.name.trim(),
-            email: signup.email.trim(),
-            password: signup.password.trim()
-        }
-        postData(newData)
-    }
 
 
     return (
-        <div className ="App">
-            <header><h2>Signup Now!</h2></header>
-            <form>
-                <input />
-            </form>
-        </div>
+        <form>
+            <div className='form-group inputs'>
+                <h4>Information</h4>
+
+                <label> Name&nbsp;
+                    <input
+                        // value={values.name}
+                        // onChange={onInputChange}
+                        name='name'
+                        type='text'
+                    />
+                </label>
+                <label>Username
+                    <input 
+                    name='username'
+                    type='text'
+                    /> 
+                </label>
+                <label>Email
+                    <input 
+                    name='email'
+                    type='email'
+                    />
+                </label>
+
+                <label>Phonenumber
+                    <input
+                        // value={values.email}
+                        // onChange={onInputChange}
+                        name='phonenumber'
+                        type='text'
+                    />
+                </label>
+
+                <label>Password
+                    <input
+                        type='text'
+                        name='password'
+                        // value={values.password}
+                        // onChange={onInputChange}
+                    />
+                </label>
+            </div>
+        </form>
     )
 }
