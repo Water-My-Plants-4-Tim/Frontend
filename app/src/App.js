6183b4cacfd4dfaa2ace40e397174acd3f40ec47
Daniel Hoesle <dhoesle21@gmail.com>
2020-07-28 18:00:49 -0600

    added context in app.js

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect, createContext } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
-
 
 import Nav from './components/Nav'
 import Signup from './components/Signup'
@@ -12,19 +10,29 @@ import Dashboard from './components/Dashboard';
 import PrivateRoute from './components/PrivateRoute'
 import Plant from './components/Plant'
 
+
+export const PlantContext = createContext();
+console.log("PlantContext", PlantContext)
+
+
+const initialPlants = []
 function App() {
-  
+  const [plants, setPlants] = useState(initialPlants)
+  console.log("App -> plants", plants)
+
   return (
-
-
     
 
     <Router>
       <div className="App">
-        <Nav />
-        {/* <Plant /> */}
+        <Nav />      
         <Switch>
-          <PrivateRoute exact path='/protected' component={Dashboard} />
+          <PlantContext.Provider
+            value={{ plants }}
+          >
+            <PrivateRoute exact path='/protected' component={Dashboard} />
+          </PlantContext.Provider>
+
           <Route  path='/login' component={Login} />
           <Route path='/' component={Signup} />
 
